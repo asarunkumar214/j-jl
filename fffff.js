9a0ec1b1a6fe7c6ad4dc21656baf74d656e5dedd
ARUNKUMAR.N <asarunkumar214@gmail.com>
2025-01-20 17:31:05 +0530

    Add files via upload

---
+++ fffff.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text",{
+    strings:["FrontEndDeveloper","BackEndDeveloper","WebDesigner","FullStackDeveloper"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
